df066e595fc3d87664eeb59feeae984bf3908f78
Tisciit <38668741+Tisciit@users.noreply.github.com>
2018-08-04 21:18:31 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,19 +12,19 @@ function setup() {
 }
 
 function modelLoaded() {
-    alert("ready");
+    alert("Classifier is ready");
     classifier.predict(camera, predictionMade)
 }
 
 function predictionMade(err, result) {
     if (err) {
+        alert("We faced an issue, sorry :(");
         console.error(err);
     } else {
         prediction = result[0].className;
         proba = result[0].probability;
+        classifier.predict(camera, predictionMade);
     }
-
-    classifier.predict(camera, predictionMade);
 }
 
 function draw() {
