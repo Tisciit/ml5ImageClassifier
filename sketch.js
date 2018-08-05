28d9bd8e42ce5dc4e32e85b5bdd80e99ea8f4e96
Tisciit <38668741+Tisciit@users.noreply.github.com>
2018-08-05 10:43:42 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let classifier;
 let camera;
+let camera_facingMode = "environment";
+let switchButton;
 
 let prediction;
 let proba;
@@ -7,16 +9,47 @@ let proba;
 let canPredict = false;
 
 function setup() {
-    camera = createCapture(VIDEO, function () {
-        classifier = ml5.imageClassifier("MobileNet", camera, modelLoaded)
-    }).parent("#wrapper");
 
-    camera.addClass("video");
+    createCanvas(innerWidth, innerHeight);
+    //Initialize capture using "environment camera", "user" would be frontcamera"
+    camera = createCapture({
+        audio: false,
+        video: {
+            facingMode: camera_facingMode
+        }
+    }, function () {
+        classifier = ml5.imageClassifier("MobileNet", camera, modelLoaded)
+    });
+
+    camera.hide();
 
     prediction = createP("Prediction").parent("#wrapper");
     proba = createP("Probability").parent("#wrapper");
+    switchButton = createButton("Switch camera").parent("#wrapper");
+    switchButton.mousePressed(switchCamera);
 
     frameRate(4);
+}
+
+function switchCamera() {
+    canPredict = false;
+
+    if (camera_facingMode == "environment") {
+        camera_facingMode = "user";
+    } else {
+        camera_facingMode = "environment";
+    }
+
+    camera = createCapture({
+        audio: false,
+        video: {
+            facingMode: camera_facingMode
+        }
+    }, function () {
+        canPredict = true;
+    });
+
+    camera.hide();
 }
 
 function modelLoaded() {
@@ -40,9 +73,9 @@ function predictionMade(err, result) {
 }
 
 function draw() {
-    console.log("draw");
     if (canPredict) {
         canPredict = false;
+        image(0, 0, camera, innerWidth, innerHeight);
         classifier.predict(camera, predictionMade);
     }
 }
